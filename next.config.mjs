632de5ec4e685e9ea5cb4d69f71489d8f78e865a
rectanglehihat <rectanglehihat@kakao.com>
2024-06-25 09:47:37 +0900

    Feat: next/image 허용 url 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,65 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.com',
+        port: '',
+      },
+      {
+        protocol: 'http',
+        hostname: '*.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.kr',
+        port: '',
+      },
+      {
+        protocol: 'http',
+        hostname: '*.kr',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.net',
+        port: '',
+      },
+      {
+        protocol: 'http',
+        hostname: '*.net',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.be',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.de',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.uk',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.io',
+        port: '',
+      },
+      {
+        protocol: 'http',
+        hostname: '*.io',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
