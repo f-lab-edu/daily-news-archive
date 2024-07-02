0e68b6fb106c652211dfea21c911f625eb52d97e
rectanglehihat <rectanglehihat@gmail.com>
2024-07-02 15:53:58 +0900

    Feat: 차트 위에 키워드 표시

--- src/components/TopKeyWordChart.tsx
+++ src/components/TopKeyWordChart.tsx
@@ -1,8 +1,9 @@
 import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
 import { useEffect } from 'react';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, ChartDataLabels);
 
 interface Data {
   labels: string[];
@@ -20,12 +21,12 @@ const data: Data = {
       label: '# of Counts',
       data: [],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.5)',
-        'rgba(54, 162, 235, 0.5)',
-        'rgba(255, 206, 86, 0.5)',
-        'rgba(75, 192, 192, 0.5)',
-        'rgba(153, 102, 255, 0.5)',
-        'rgba(255, 159, 64, 0.5)'
+        'rgba(255, 99, 132, 0.7)',
+        'rgba(54, 162, 235, 0.7)',
+        'rgba(255, 206, 86, 0.7)',
+        'rgba(75, 192, 192, 0.7)',
+        'rgba(153, 102, 255, 0.7)',
+        'rgba(255, 159, 64, 0.7)'
       ]
     }
   ]
@@ -34,12 +35,25 @@ const data: Data = {
 const options = {
   responsive: true,
   plugins: {
-    legend: {
-      position: 'top' as const
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Doughnut Chart'
+    datalabels: {
+      color: '#fff',
+      formatter: (value: number, ctx: any) => {
+        let sum = 0;
+        let dataArr = ctx.chart.data.datasets[0].data;
+        dataArr.map((data: number) => {
+          sum += data;
+        });
+        let percentage = ((value * 100) / sum).toFixed(2) + '%';
+        return ctx.chart.data.labels[ctx.dataIndex] + '\n' + percentage;
+      },
+      font: {
+        weight: 'bold' as const,
+        size: 12
+      },
+      anchor: 'center' as const,
+      align: 'center' as const,
+      offset: 0,
+      rotation: 0
     }
   }
 };
