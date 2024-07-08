7c819e3f14ada4e537410cf019c56be571aa3582
rectanglehihat <rectanglehihat@gmail.com>
2024-07-09 02:30:10 +0900

    Refactor: TopKeyWordChart topKeywords 변경시에만 도넛 차트 data 업데이트

--- src/components/TopKeyWordChart.tsx
+++ src/components/TopKeyWordChart.tsx
@@ -1,36 +1,9 @@
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
-import { useEffect } from 'react';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { useMemo } from 'react';
 
 ChartJS.register(ArcElement, Tooltip, ChartDataLabels);
-
-interface Data {
-  labels: string[];
-  datasets: {
-    label: string;
-    data: number[];
-    backgroundColor: string[];
-  }[];
-}
-
-const data: Data = {
-  labels: [],
-  datasets: [
-    {
-      label: '# of Counts',
-      data: [],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.7)',
-        'rgba(54, 162, 235, 0.7)',
-        'rgba(255, 206, 86, 0.7)',
-        'rgba(75, 192, 192, 0.7)',
-        'rgba(153, 102, 255, 0.7)',
-        'rgba(255, 159, 64, 0.7)'
-      ]
-    }
-  ]
-};
 
 interface TopKeyWord {
   keyword: string;
@@ -38,16 +11,39 @@ interface TopKeyWord {
 }
 
 interface TopKeyWordChartProps {
-  activeTab: string;
   topKeywords: TopKeyWord[];
   onClick: (word: string) => void;
 }
 
-const TopKeyWordChart = ({
-  activeTab,
-  topKeywords,
-  onClick
-}: TopKeyWordChartProps) => {
+const TopKeyWordChart = ({ topKeywords, onClick }: TopKeyWordChartProps) => {
+  const { labels, datasets } = useMemo(() => {
+    const keywords: string[] = [];
+    const counts: number[] = [];
+
+    topKeywords.forEach(item => {
+      keywords.push(item.keyword);
+      counts.push(item.count);
+    });
+
+    return {
+      labels: keywords,
+      datasets: [
+        {
+          label: '# of Counts',
+          data: counts,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+            'rgba(255, 159, 64, 0.7)'
+          ]
+        }
+      ]
+    };
+  }, [topKeywords]);
+
   const options = {
     responsive: true,
     plugins: {
@@ -69,34 +65,14 @@ const TopKeyWordChart = ({
     onClick: (e: any, item: any) => {
       if (item.length === 0) return;
       const index = item[0].index;
-      const keyword = data.labels[index];
+      const keyword = labels[index];
       onClick(keyword);
     }
   };
 
-  const splitArrayByKey = (data: TopKeyWord[]) => {
-    const keywords: string[] = [];
-    const counts: number[] = [];
-
-    data.forEach(item => {
-      keywords.push(item.keyword);
-      counts.push(item.count);
-    });
-
-    return { keywords, counts };
-  };
-
-  useEffect(() => {
-    const { keywords, counts } = splitArrayByKey(topKeywords);
-    data.labels = keywords;
-    data.datasets.map(dataset => {
-      dataset.data = counts;
-    });
-  }, [activeTab]);
-
   return (
     <>
-      <Doughnut data={data} options={options} />
+      <Doughnut data={{ labels, datasets }} options={options} />
     </>
   );
 };
