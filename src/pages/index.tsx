555db481ba89d1e057006fc779ea39130c03c88b
rectanglehihat <rectanglehihat@gmail.com>
2024-07-09 13:20:03 +0900

    Feat: api key env로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,35 @@ import {
 } from '@tanstack/react-query';
 import { useState } from 'react';
 
-const fetchNews = async (category: string) => {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const fetchNews = async (category: string): Promise<NewsData[]> => {
   const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&pageSize=100&apiKey=5369507a287b4e68b8f709bf96346024`
+    `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&pageSize=100&apiKey=${API_KEY}`
   );
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
-  return response.json();
+  const data = await response.json();
+  return data.articles.map(formatArticle);
+};
+
+const formatArticle = (article: NewsData): NewsData => {
+  if (!article.urlToImage) return article;
+  if (
+    article.urlToImage.startsWith('http') ||
+    article.urlToImage.startsWith('https')
+  ) {
+    return article;
+  }
+  return { ...article, urlToImage: null };
 };
 
 export async function getServerSideProps() {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['posts'],
+    queryKey: ['news', 'business'],
     queryFn: () => fetchNews('business')
   });
 
@@ -41,39 +55,22 @@ export default function Home({ dehydratedState }: HomeProps) {
   const [activeTab, setActiveTab] = useState<string>('business');
 
   const { data, error, isLoading } = useQuery({
-    queryKey: ['getNews', activeTab],
+    queryKey: ['news', activeTab],
     queryFn: () => fetchNews(activeTab)
   });
 
-  const formatArticleImage = () => {
-    return data.articles.map((article: NewsData) => {
-      if (!article.urlToImage) return article;
-
-      if (
-        article.urlToImage?.startsWith('http') ||
-        article.urlToImage?.startsWith('https')
-      ) {
-        return article;
-      }
-
-      return { ...article, urlToImage: null };
-    });
-  };
-
   if (isLoading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error: {error.message}</h1>;
+  if (error instanceof Error) return <h1>Error: {error.message}</h1>;
 
   return (
     <main className="p-5">
-      <h1 className="text-center text-lg fonft-bold pb-5">
-        Daily News Archive
-      </h1>
+      <h1 className="text-center text-lg font-bold pb-5">Daily News Archive</h1>
 
       <HydrationBoundary state={dehydratedState}>
         <Tabs
           activeTab={activeTab}
           setActiveTab={setActiveTab}
-          newsList={formatArticleImage()}
+          newsList={data || []}
         />
       </HydrationBoundary>
     </main>
